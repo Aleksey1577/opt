a3e0652f07bde3f1d16b5dde89f4a9e2996bee6d
Aleksey1577 <alekseykras94@gmail.com>
2024-06-14 19:23:53 +0300

    Updates

--- _nuxt/C_Pih9Sj.js
+++
@@ -1,1 +0,0 @@
-import{openBlock as t,createElementBlock as o,createVNode as r,createElementVNode as e}from"vue";import{H as a}from"./DFMbmlmx.js";import"./Cawqb1VY.js";import"vue-router";const c=e("div",{class:"max-w-[1200px] mx-auto p-6"},[e("h1",{class:"text-2xl font-bold mb-4"},"Контакты")],-1),i={__name:"contacts",setup(m){return(s,n)=>(t(),o("div",null,[r(a),c]))}};export{i as default};
